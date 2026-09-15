3959a6c506d1b253a061f69f5b9ca18a5fbf2c2a
Adeyinka Adedokun <deyinka.adedokun@gmail.com>
2026-09-15 23:19:21 +0100

    Update AuthContext.tsx

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -283,7 +283,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       value={{
         user,
         providerProfile,
-        isAuthenticated: !!user,
+        isAuthenticated: !!user && user.status !== 'SUSPENDED',
         isLoading,
         authError,
         isAuthModalOpen,
